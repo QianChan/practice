cb539eee88095a2f329bf5ed49135cb4f4a147d1
QianChan <cq3213245@163.com>
2016-03-12 22:46:29 +0800

    test

--- QianChan/js/index.js
+++
@@ -1,8 +0,0 @@
-var back = document.getElementById("#backToTop");
-var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
-window.onscroll = function(){
-    back.style.display = "block";
-}
-top.onclick = function(){
-    back.style.display = "none";
-}
